6085321645189f6deeb34fbb00b7bd980b4c8f74
Isaac Medeiros <imedeiros@morphus.com.br>
2021-04-24 15:26:08 -0300

    componentes com TypeScript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,14 +4,14 @@ import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
 }
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Array<Repository>>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
